1d4c715e6ac5a97ac073c34131a3ea4c210b255b
Болдин Дмитрий Вадимович <dimkahs658@gmail.com>
2023-12-12 18:34:38 +0300

    намечена структура main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/spi.h>
+
+
+void SPI()
+  {
+
+
+  } 
+void UART()
+  {
+
+  } 
+
 int main()
 {
+
+
 
 
 
