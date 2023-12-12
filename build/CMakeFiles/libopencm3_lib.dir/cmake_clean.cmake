193b7e24c35848136feeac790837bd83eac84978
Антон Куриленко <bozzzzik4@gmail.com>
2023-12-12 17:52:33 +0300

    Персонализация проекта

---
+++ build/CMakeFiles/libopencm3_lib.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libopencm3_lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/libopencm3_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mk_23.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mk_23.dir/src/main.cpp.obj"
+  "CMakeFiles/mk_23.dir/src/main.cpp.obj.d"
+  "mk_23"
+  "mk_23.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mk_23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
